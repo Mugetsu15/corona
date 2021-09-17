8a0ccadbbae1298b1ffdc7a154d9150e73e4d6f7
MSchmitz <m.schmitz@maxuniverse.de>
2021-09-17 16:46:42 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,10 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
+# Set credentials
+git config --global user.email "${{ secrets.USER_EMAIL }}"
+git config --global user.name "${{ secrets.USER_NAME }}"
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Mugetsu15/corona.git master:gh-pages
 
